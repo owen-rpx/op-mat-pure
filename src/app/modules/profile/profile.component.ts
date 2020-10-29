5a41d664d0a2726646a6b40f28be2f8e9ceb8de8
owen-rpx <xudongz@rocketsoftware.com>
2020-10-29 16:25:19 +0800

    init

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { Component } from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Group1',
+    children: [
+      {
+        name: 'APP1-1',
+        children: [{
+          name: 'ReleaseV1'
+        }, {
+          name: 'ReleaseV2'
+        }]
+      },
+      {
+        name: 'APP1-2',
+        children: [{
+          name: 'ReleaseV1'
+        }, {
+          name: 'ReleaseV2'
+        }]
+      }
+    ]
+  },
+  {
+    name: 'Group2',
+    children: [
+      {
+        name: 'APP2-1',
+        children: [{
+          name: 'ReleaseV1'
+        }, {
+          name: 'ReleaseV2'
+        }]
+      },
+      {
+        name: 'APP2-2',
+        children: [{
+          name: 'ReleaseV1'
+        }, {
+          name: 'ReleaseV2'
+        }]
+      }
+    ]
+  }
+];
+
+/**
+ * @title Tree with nested nodes
+ */
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.dataSource.data = TREE_DATA;
   }
 
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+
+  goLeaf(name): void {
+    console.log('go leaf ' + name);
+  }
 }
